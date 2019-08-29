14505d408c490ff20dde6aeb66dc3c11839adaaa
Olafur Arason <olafura@olafura.com>
2019-08-28 23:49:38 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExXml.MixProject do
   def project do
     [
       app: :ex_xml,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
