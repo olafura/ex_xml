4addd84e7e13e49fb366e427ee0016ad5c70040c
Olafur Arason <olafura@olafura.com>
2019-09-08 15:45:56 -0400

    Add basic documentation

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule ExXml.MixProject do
   defp deps do
     [
       {:nimble_parsec, "~> 0.5"},
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 
